40d796224b2dbbcdc81958df00006ecfa1600f0b
Andrew Valley <andyvalley@live.com>
2021-02-22 12:17:13 -0500

    fixed break

--- MonsterTools.py
+++ MonsterTools.py
@@ -5,3 +5,4 @@ import os
 os.system('cd ' + os.path.join(os.environ["HOMEPATH"], "gE_Ode", "MonsterTools") + ' && git pull')
 os.system(os.path.join(os.environ["HOMEPATH"], "gE_Ode", "MonsterTools", "output", "app", "app.exe"))
 
+
