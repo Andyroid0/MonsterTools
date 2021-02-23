afd2c887e7030e96c27e582d11a5be4788ea421d
Andrew Valley <andyvalley@live.com>
2021-02-22 21:28:52 -0500

    toffee coffeee

--- MonsterTools.py
+++ MonsterTools.py
@@ -1,3 +1,7 @@
 #! python3
 
-import init
+import os
+
+os.system('cd ' + os.path.join(os.environ["HOMEPATH"], "gE_Ode", "MonsterTools") + ' && git pull')
+os.system(os.path.join(os.environ["HOMEPATH"], "gE_Ode", "MonsterTools", "output", "app", "app.exe"))
+
